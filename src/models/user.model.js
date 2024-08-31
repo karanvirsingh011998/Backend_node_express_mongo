7ecb5d4b64a8cc244942438a1b334ef5573d5fd4
unknown <Karanvir011998@gmail.com>
2024-08-31 21:37:43 +0530

    feat: added schmeas and methods to the schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,13 +50,30 @@ userSchema.pre("save", async function (next) {
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password);
-  };
-  userSchema.methods.generateAccessToken = async function (password) {
-    return await bcrypt.compare(password, this.password);
-  };
+  return await bcrypt.compare(password, this.password);
+};
 
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    userName: this.userName,
+    fullName: this.fullName,
+  },
 
+process.env.ACCESS_TOKEN_SECRET,{
+  expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+});
+};
 
+userSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign({
+    _id: this._id,
+  },
+
+process.env.REFRESH_TOKEN_SECRET,{
+  expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+});
+};
 
 export const User = mongoose.model("User", userSchema);
